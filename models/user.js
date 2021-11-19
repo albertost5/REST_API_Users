c1057de46354d6347c94311b8aba9e7657860653
albertost5 <sancheztorresalberto@gmail.com>
2021-11-19 17:59:04 +0100

    Created user model and function.

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true},
+    status: { type: Boolean, default: true},
+    image: { type: String, required: false}
+});
+
+module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 // MODULES
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+
 
 // MIDDLEWARES
-router.use(express.json());
+// router.use(express.json());
 
 
 // ROUTES
@@ -11,4 +13,42 @@ router.get('/', (req, res) => {
     res.json('Ready first users endpoint');
 });
 
+router.post('/', (req, res) => {
+
+    let body = req.body;
+    let result = createUser(body);
+
+    result.then(u => {
+
+        res.json(u)
+
+    }).catch(err => {
+
+        res.status(400).json({
+            'code': 400,
+            'message': 'BAD_REQUEST',
+            'description': 'Bad request due to malformed sintax.'
+        })
+
+    });
+});
+
+
+
+
+
+// FUNCTIONS
+async function createUser(body) {
+    let user = new User({
+        email: body.email,
+        name: body.name,
+        password: body.password
+    });
+
+    return await user.save();
+}
+
+
+
+// EXPORT
 module.exports = router;
