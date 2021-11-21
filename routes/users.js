7efdd13322a4ff88046b53b16a7707334b8afbae
albertost5 <sancheztorresalberto@gmail.com>
2021-11-21 16:36:32 +0100

    Added validation for users methods: Create and Upate.

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,25 @@ const express = require('express');
 const user = require('../models/user');
 const router = express.Router();
 const User = require('../models/user');
+const Joi = require('joi');
 
 
 // MIDDLEWARES
 // router.use(express.json());
+
+// SCHEMA
+const schema = Joi.object({
+	name: Joi.string()
+		.min(3)
+		.max(10)
+		.required(),
+
+	password: Joi.string()
+		.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+	email: Joi.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+});
 
 
 // ROUTES
@@ -29,8 +44,16 @@ router.post('/', async (req, res) => {
 
 	let body = req.body;
 	// let result = createUser(body);
+	const { error, value } = schema.validate({ name: body.name, email: body.email });
+
+	if(error) return res.status(400).json({
+		'code': 400,
+		'message': 'BAD_REQUEST',
+		'description': error.details[0].message.replace(/\"/g, '').toUpperCase()
+	});
 
 	let result;
+
 	try {
 		result = await createUser(body);
 	} catch (err) {
@@ -46,6 +69,15 @@ router.post('/', async (req, res) => {
 
 router.put('/:email', async (req, res) => {
 	const EMAIL = req.params.email;
+
+	const { error, value } = schema.validate({ name: req.body.name });
+
+	if(error) return res.status(400).json({
+		'code': 400,
+		'message': 'BAD_REQUEST',
+		'description': error.details[0].message.replace(/\"/g, '').toUpperCase()
+	});
+
 	let user;
 
 	try {
